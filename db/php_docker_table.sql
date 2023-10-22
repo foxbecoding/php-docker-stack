940627a08284507d8e4b598c98612457dd99bb4e
FoxBeCoding <fox@foxbecoding.com>
2023-10-21 19:13:21 -0500

    updated app files

---
+++ db/php_docker_table.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: db:3306
+-- Generation Time: Oct 21, 2023 at 10:48 PM
+-- Server version: 8.1.0
+-- PHP Version: 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php_docker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `php_docker_table`
+--
+
+CREATE TABLE `php_docker_table` (
+  `id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `php_docker_table`
+--
+
+INSERT INTO `php_docker_table` (`id`, `title`, `body`, `created_at`) VALUES
+(1, 'first post', 'first post message', '2023-10-21 22:45:20'),
+(2, 'second post', 'second post message', '2023-10-21 22:46:10');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `php_docker_table`
+--
+ALTER TABLE `php_docker_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `php_docker_table`
+--
+ALTER TABLE `php_docker_table`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
